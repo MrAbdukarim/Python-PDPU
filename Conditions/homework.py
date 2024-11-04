fdbe0443dcb85595e96ec6c2be833f7db01a1bf4
Abdukarim Qarshiyev <mr.abdukarim1@gmail.com>
2024-11-04 22:39:46 +0500

    update

--- Conditions/homework.py
+++ Conditions/homework.py
@@ -23,3 +23,16 @@ if 1 <= grade <= 11:
             print(f'Siz {grade}-sinfsiz va ballingiz {score}/100. Sizning natijangiz qoniqarsiz.')
 else:
     print("Xatolik bor!")
+
+# 2-vazifa
+car_year = int(input("Avtomobilni yilini kiriting: "))
+driver_age = int(input("Yoshingizni kiriting: "))
+
+if car_year >= 2020:
+    cost = 500 if driver_age > 25 else 700
+elif car_year >= 2015:
+    cost = 600 if driver_age > 25 else 800
+else:
+    cost = 700 if driver_age > 25 else 900
+
+print(f"Avtomobilingiz yili {car_year}.\n Sug'urta narxi: {cost}$")
