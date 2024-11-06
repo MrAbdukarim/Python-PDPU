a01eb66b22b4bbd4e354b07f419257ece8fe8045
Abdukarim Qarshiyev <mr.abdukarim1@gmail.com>
2024-11-06 10:47:18 +0500

    update

--- While.py
+++ While.py
@@ -2,27 +2,53 @@
 ## By MrAbdukarim 11/5/2024
 #
 #
+#
+# # 1-vazifa
 # num = int(input("Raqam kiriting (0 kiritilganda tugaydi): "))
+#
+# while not num == 0:
+#     num2 = int(input("Raqam kiriting (0 kiritilganda tugaydi): "))
+#     if num > 0:
+#         num += num2
+#         print(num)
+#     if num2 == 0:
+#         break
+#         print(num)
+#
+# # 2-vazifa
+# num_a = int(input("A soni kiriting: "))
+# num_b = int(input("B soni kiriting: "))
 # summa = 0
 #
-# while num > 0:
-#     num2 = int(input("Raqam kiriting (0 kiritilganda tugaydi): "))
-#     summa += num + num2
-#     print("Yig'indi:", summa)
-#     if num2 > 0:
-#         summa += num2
-# else:
-#     print(summa)
+# if num_a > num_b:
+#     num_a, num_b = num_b, num_a
+#
+# while num_a <= num_b:
+#     summa += num_a
+#     num_a += 1
+# print(summa)
+#
+# # 3-vazifa
+# numbers = []
+#
+# while len(numbers) < 5:
+#     num = int(input("Raqam kiriting: "))
+#     numbers.append(num)
+#     numbers.sort()
+#
+# print("Kiritilgan raqamlar listi:", numbers)
+#
+# # 4-vazifa
+# x = [1, 2, 3, 14, 5, 6, 6, 7]
+# max_element = x[0]
+# i = 1
+#
+# while i < len(x):
+#     if x[i] > max_element:
+#         max_element = x[i]
+#     i += 1
+#
+# print("Eng katta element:", max_element)
 
-# 2-vazifa
-num_a = int(input("A soni kiriting: "))
-num_b = int(input("B soni kiriting: "))
-summa = 0
-
-if num_a > num_b:
-    num_a, num_b = num_b, num_a
-
-while num_a <= num_b:
-    summa += num_a
-    num_a += 1
-print(summa)
+# 5-vazifa
+x = [1, 2, 3, 14, 5, 6, 6, 7]
