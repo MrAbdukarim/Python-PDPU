ddbf5c1ce4a5b82c46908ec7100dea1714796ecb
Abdukarim Qarshiyev <mr.abdukarim1@gmail.com>
2024-11-06 11:40:53 +0500

    update

--- While.py
+++ While.py
@@ -49,6 +49,41 @@
 #     i += 1
 #
 # print("Eng katta element:", max_element)
-
-# 5-vazifa
-x = [1, 2, 3, 14, 5, 6, 6, 7]
+#
+# # 5-vazifa
+# x = [1, 2, 3, 14, 5, 6, 6, 7]
+# max_element = x[0]
+# max_index = 0
+# i = 1
+#
+# while i < len(x):
+#     if x[i] > max_element:
+#         max_element = x[i]
+#         max_index = i
+#     i += 1
+#
+# print("Eng katta element indexi:", max_index)
+#
+# # 6-vazifa
+# num = input("Son kiriting: ")
+# summa = 0
+#
+# while summa < len(num):
+#     summa += 1
+#
+# print("Xonalar soni:", summa)
+#
+# # 7-vazifa
+# x = [1, 2, 0, 14, 5, -6]
+# max_element = x[0]
+# min_element = x[0]
+# summa = 1
+#
+# while summa < len(x):
+#     if x[summa] > max_element:
+#         max_element = x[summa]
+#     if x[summa] < min_element:
+#         min_element = x[summa]
+#     summa += 1
+#
+# print(f"Eng katta element: {max_element}, Eng kichik element: {min_element}")
